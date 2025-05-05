c08382677c142eaa469d0f5f160c240c7d4482a1
abieekml <abii.kamal14@gmail.com>
2025-05-05 19:09:47 +0700

    Update kata terpanjang.py

--- kata terpanjang.py
+++ kata terpanjang.py
@@ -7,11 +7,11 @@ def cari_kata_terpanjang():
     
     if not kata:
         return "Kalimat kosong."
-    
+        
     # Mencari kata terpanjang
     kata_terpanjang = max(kata, key=len)
     
     return f"Kata terpanjang adalah '{kata_terpanjang}' dengan {len(kata_terpanjang)} karakter."
-
+    
 # Menjalankan fungsi
 print(cari_kata_terpanjang())
